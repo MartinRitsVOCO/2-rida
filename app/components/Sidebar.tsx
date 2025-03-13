839c7aa50681aece29fe6a79a9e35569287168d4
MartinRitsVOCO <martin.ritsberg@voco.ee>
2025-03-13 14:30:41 +0200

    Finished the sidebar
    Co-authored-by: reinarkajava <reinarkajava@users.noreply.github.com>
    Co-authored-by: Juss <gbrds@users.noreply.github.com>

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,74 @@
+'use client'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const Sidebar = () => {
+    const path = usePathname();
+    const [navTitle, setNavTitle] = useState("");
+    const pages = [
+        {
+            name: "Home",
+            url: "/",
+        },
+        {
+            name: "Email Please",
+            url: "/email-please",
+        },
+    ];
+    const [links, setLinks] = useState<React.ReactNode[]>([]);
+
+    useEffect(() => {
+        const newLinks: React.ReactNode[] = [];
+        pages.forEach((page) => {
+            if (page.url === path) {
+                newLinks.push(
+                    <div
+                        className="text-blue-200"
+                        key={page.url}
+                    >
+                        {page.name}
+                    </div>
+                );
+            } else {
+                newLinks.push(
+                    <Link
+                        key={page.url}
+                        href={page.url}
+                        className="text-gray-500 hover:text-gray-300"
+                    >
+                        {page.name}
+                    </Link>
+                );
+            }
+        });
+        setLinks(newLinks);
+
+        switch (path) {
+            case "/":
+                document.title = "Home";
+                setNavTitle("Home");
+                break;
+            case "/email-please":
+                document.title = "Email Please";
+                setNavTitle("Email Please");
+                break;
+            default:
+                document.title = "Page not found";
+                setNavTitle("Page not found");
+                break;
+        }
+    }, [path]);
+
+    return (
+        <div className="w-64 bg-gray-800 text-white h-screen">
+            <div className="flex justify-center items-center py-4 font-bold text-2xl border-4 border-gray-700">
+                {navTitle}
+            </div>
+            <div className="flex flex-col justify-around items-center py-6 px-3 h-1/3 min-h-50 font-bold text-xl border-4 border-gray-700">
+                {links.map((link) => link)}
+            </div>
+        </div>
+    );
+};
+export default Sidebar;
---
+++ app/email-please/page.tsx
@@ -0,0 +1,19 @@
+const page = () => {
+  return (
+    <div>
+        <div>
+            <p className="text-8x1">A</p>
+        </div>
+        <div className="space-x-25">
+            <div>
+                <button className="bg-green-500 hover:bg-green-600 w-24 h-24 rounded-full py-3 px-6"> Kiri on oige </button>
+            </div>
+
+           
+            <button className="bg-red-500 hover:bg-red-600 w-24 h-24"> Kiri on vale </button>
+        </div>
+ 
+    </div>
+  )
+}
+export default page
